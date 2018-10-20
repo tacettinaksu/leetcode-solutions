ea015a5dcaa8833924d7e65439a52c5ad923fee8
Tacettin AKSU <tacettin.aksu@gmail.com>
2018-10-20 13:37:31 +0300

    add empty class for ValidAnagram case

---
+++ src/main/java/com/techmisal/medium/ValidAnagram.java
@@ -0,0 +1,19 @@
+package com.techmisal.medium;
+
+/**
+ * An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+ * typically using all the original letters exactly once.[1]
+ * <p>
+ * For example,
+ * the word "anagram" can be rearranged into "nag a ram",
+ * or the word "binary" into "brainy".
+ * <p>
+ * <p>
+ * Reference: https://en.wikipedia.org/wiki/Anagram
+ */
+public class ValidAnagram {
+
+    public boolean isValidAnagram(String originalWord, String anagram) {
+        return false;
+    }
+}
