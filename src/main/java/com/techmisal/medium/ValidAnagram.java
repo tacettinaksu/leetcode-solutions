727ba528f6c1c1dfa1e501297458ca099bfdc2c6
Tacettin AKSU <tacettin.aksu@gmail.com>
2018-10-20 13:47:22 +0300

    implementation for anagram

--- src/main/java/com/techmisal/medium/ValidAnagram.java
+++ src/main/java/com/techmisal/medium/ValidAnagram.java
@@ -1,5 +1,7 @@
 package com.techmisal.medium;
 
+import java.util.Arrays;
+
 /**
  * An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
  * typically using all the original letters exactly once.[1]
@@ -14,6 +16,23 @@ package com.techmisal.medium;
 public class ValidAnagram {
 
     public boolean isValidAnagram(String originalWord, String anagram) {
-        return false;
+        String removeSpacesOriginalWord = originalWord.replaceAll(" ", "");
+        String removeSpacesAnagram = anagram.replaceAll(" ", "");
+
+        if (removeSpacesAnagram.length() != removeSpacesOriginalWord.length()) {
+            return false;
+        }
+
+        String sortedOriginalWord = sortString(removeSpacesOriginalWord.toLowerCase());
+        String sortedAnagram = sortString(removeSpacesAnagram.toLowerCase());
+
+        return sortedOriginalWord.equals(sortedAnagram);
     }
+
+    private String sortString(String inputString) {
+        char tempArray[] = inputString.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
+
 }
